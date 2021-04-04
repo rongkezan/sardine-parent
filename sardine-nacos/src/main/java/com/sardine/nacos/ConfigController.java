dae5b50864fe70e8ea2a98a794d62dcfd53aaf92
rongkezan <kezan.r@renrenbit.com>
2021-04-04 19:27:38 +0800

    nacos

---
+++ sardine-nacos/src/main/java/com/sardine/nacos/ConfigController.java
@@ -0,0 +1,27 @@
+package com.sardine.nacos;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author keith
+ */
+@RestController
+@RequestMapping("config")
+@RefreshScope
+public class ConfigController {
+
+    @Value("${common.name}")
+    private String name;
+
+    @Value("${common.age}")
+    private Integer age;
+
+    @GetMapping("get")
+    public String get(){
+        return name + "," + age;
+    }
+}
---
+++ sardine-nacos/src/main/java/com/sardine/nacos/NacosApplication.java
@@ -0,0 +1,16 @@
+package com.sardine.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Nacos Application
+ *
+ * @author keith
+ */
+@SpringBootApplication
+public class NacosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosApplication.class, args);
+    }
+}

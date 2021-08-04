6b6e607cf48df74e320ef919daa8dec1364336b1
rongkezan <kezan.r@renrenbit.com>
2021-08-04 16:17:50 +0800

    redis

--- sardine-redis/src/main/java/com/sardine/redis/RedisConfig.java
+++ sardine-redis/src/main/java/com/sardine/redis/RedisConfig.java
@@ -3,23 +3,22 @@ package com.sardine.redis;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sardine.utils.SpringContextUtils;
-import com.sardine.utils.exception.SystemException;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.config.Config;
+import org.redisson.config.SentinelServersConfig;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Redis Config
@@ -51,8 +50,42 @@ public class RedisConfig {
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://" + redisProperties.getHost() + ":" + redisProperties.getPort());
         config.setCodec(new JsonJacksonCodec());
+        RedisProperties.Cluster cluster = redisProperties.getCluster();
+        RedisProperties.Sentinel sentinel = redisProperties.getSentinel();
+        if (cluster != null) {
+            clusterConfig(config, cluster);
+        } else if (sentinel != null) {
+            sentinelConfig(config, sentinel);
+        } else {
+            singleConfig(config);
+        }
         return Redisson.create(config);
     }
+
+    private void clusterConfig(Config config, RedisProperties.Cluster cluster){
+        List<String> nodes = cluster.getNodes();
+        config.useClusterServers()
+                .addNodeAddress(nodes.toArray(new String[0]))
+                .setConnectTimeout((int) redisProperties.getTimeout().toMillis())
+                .setPassword(redisProperties.getPassword());
+    }
+
+    private void sentinelConfig(Config config, RedisProperties.Sentinel sentinel){
+        List<String> nodes = sentinel.getNodes();
+        SentinelServersConfig sentinelConfig = config.useSentinelServers().setMasterName(sentinel.getMaster());
+        for (String node : nodes) {
+            sentinelConfig.addSentinelAddress("redis://" + node);
+        }
+    }
+
+    private void singleConfig(Config config){
+        String prefix = redisProperties.isSsl() ? "rediss://" : "redis://";
+        config.useSingleServer()
+                .setAddress(prefix + redisProperties.getHost() + ":" + redisProperties.getPort())
+                .setConnectTimeout((int) redisProperties.getTimeout().toMillis())
+                .setDatabase(redisProperties.getDatabase())
+                .setPassword(redisProperties.getPassword());
+    }
+
 }

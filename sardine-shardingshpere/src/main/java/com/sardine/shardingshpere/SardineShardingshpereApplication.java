102938ceebe56f9e44aed0f6d43c90abb02753d0
Keith <rkz@ytbig.onaliyun.com>
2023-05-04 11:25:58 +0800

    Sharding JDBC

---
+++ sardine-shardingshpere/src/main/java/com/sardine/shardingshpere/SardineShardingshpereApplication.java
@@ -0,0 +1,12 @@
+package com.sardine.shardingshpere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SardineShardingshpereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SardineShardingshpereApplication.class, args);
+    }
+}
